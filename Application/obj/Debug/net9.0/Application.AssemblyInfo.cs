186097e0bc385514c980d41e3cbb0ea16d07f387
Matt McMeans <mrmcmans@memphis.edu>
2025-06-06 15:48:35 -0500

    add system attributes

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e5b1f0cd0b3980e48d7611e79ce81901f3048e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fe27399493679af9c81b698be800e4fdb67ed38")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
